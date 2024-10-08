09a742296ad57fa23e9bbecd883ef560ef196163
ehlkristofhenrik <39805780+ehlkristofhenrik@users.noreply.github.com>
2024-10-08 14:51:24 +0100

    Changed source script

--- static/source.sh
+++ static/source.sh
@@ -1,8 +1,15 @@
+FIRST_RUN=1
+
 function firewall() {
-    ls
-    if [ $? -ne 0 ]; then
+    if [ $FIRST_RUN -eq 1 ];then
+        FIRST_RUN=0
+        return
+    fi
+
+    ./bonfire-client "$BASH_COMMAND"
+    if [[ $? -ne 0 ]]; then
         echo "You shall not pass!"
-        exit
+        exit 1
     fi
 }
 
